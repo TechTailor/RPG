972d484397630f6818d50acf523115c22b9680d0
TechTailor <hello@moinuddin.info>
2018-01-31 18:36:57 +0530

    Resolve for Auto Discovery Issue

---
+++ src/Facade/RPG.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace TechTailor\RPG\Facade;
+
+use Illuminate\Support\Facades\Facade;
+
+class RPG extends Facade {
+    /**
+     * Get the binding in the IoC container
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'rpg'; // the IoC binding.
+    }
+}
